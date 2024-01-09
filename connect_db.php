b0438a0d42bbadcc262d7ca387984b910aeada42
unknown <ikhupy>
2024-01-10 01:06:23 +0800

    l

--- connect_db.php
+++ connect_db.php
@@ -1,5 +1,5 @@
 <?php 
     $db_username = "root";
     $db_password = "";
-    $connect = new PDO ("mysql:host=127.0.0.1; dbname=room; port=3306", $db_username);
+    $connect = new PDO ("mysql:host=127.0.0.1; dbname=room; port=3307", $db_username);
 ?>
