feb1a39a696f186bd54f26056320b544bb45bb3c
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-01-15 02:25:20 +0800

    ;

--- get_all_room_schedule_data.php
+++ get_all_room_schedule_data.php
@@ -2,17 +2,14 @@
 include 'connect_db.php';
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $token = $_SESSION['token']
-    $query = "SELECT code, floor_level, has_projector, seat_count, 'type' FROM room";
-    $query3 = "SELECT email FROM teachers WHERE token = ?";
-    $stmt1 = $connect->query($query3);
+    $token = $_SESSION['token'];
+    $query = "SELECT code, floor_level, has_projector, seat_count, 'type' FROM room;";
     $stmt = $connect->query($query);
-    if ($stmt && $stmt1) {
+    if ($stmt) {
         $data = array();
-        while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) && ($row3 = $stmt1->fetch(PDO::FETCH_ASSOC))) {
+        while (($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
             $code = $row['code'];
-            $email = $row3['email'];
-            $query2 = "SELECT day_of_week, start_time, end_time, is_restricted, token, subject_id FROM preprocessed_room_schedule WHERE room_code = ?";
+            $query2 = "SELECT date_of_week, start_time, end_time, token, subject_id FROM preprocessed_room_schedule WHERE room_code = ?;";
             $stmt2 = $connect->prepare($query2);
             $stmt2->execute([$code]);
             $roomScheduleData = array();
@@ -22,8 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                     'start_time' => $row2['start_time'],
                     'end_time' => $row2['end_time'],
                     'token' => $row2['token'],
-                    'subject_id' => $row2['subject_id'],
-                    'is_restricted' => $row2['is_restricted']
+                    'subject_id' => $row2['subject_id']
                 );
             };
             $data[] = array(
--- scripts/arrangerKit.js
+++ scripts/arrangerKit.js
@@ -18,7 +18,56 @@ function arrangeSchedule() {
         outputSection.parentNode.replaceChild(parentDiv, outputSection);
     }
     callTeacherSchedule();
-}
+};
+function roomSection() {
+    if (roomScheduleData) {
+        var roomSection = document.getElementById('roomSection');
+        roomSection.innerHTML = '';
+        roomSection.classList.add('output');
+        var room_code = roomScheduleData[roomCounter]['room_code'];
+        var floor_level = roomScheduleData[roomCounter]['floor_level'];
+        var has_projector = roomScheduleData[roomCounter]['has_projector'];
+        if (has_projector == 1) {
+            has_projector = 'Yes';
+        } else if (has_projector == 0) {
+            has_projector = 'No';
+        }
+        var seat_count = roomScheduleData[roomCounter]['seat_count'];
+    
+        var htmlContent = `
+            <h2>Room Schedule</h2>
+            <div class='roomData' class='stick-main'>
+                <button onclick='decreRoomCount()'>prev</button>
+                <p>Room: ${room_code}</p>
+                <p>Level: ${floor_level}</p>
+                <p>Projector: ${has_projector}</p>
+                <p>Seat count: ${seat_count}</p>
+                <button onclick='increRoomCount()'>next</button>
+            </div>
+            <form class="rs-formtable" id="rs-formtable" action="" method="post">
+                <table>
+                    <tr>
+                        <th>Day</th>`;
+        var daysOfWeekCUT = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        daysOfWeekCUT.forEach(function (day, index) {
+            htmlContent += `<th class='dayofweekheader' onclick='selectColumn(${index})'>${day}</th>`;
+        });
+        htmlContent += `</tr>`;
+        var timeSlots = generateTimeSlots();
+        timeSlots.forEach(function (timeSlot) {
+            htmlContent += `<tr>
+                                <td>${timeSlot}</td>`;
+            daysOfWeek.forEach(function (day) {
+                htmlContent += `<td value='${day} - ${timeSlot}'></td>`;
+            });
+            htmlContent += `</tr>`;
+        });
+        htmlContent += `</table></form>
+        <div></div>
+        <div id='tbl'></div>`;
+        roomSection.innerHTML = htmlContent;
+    }
+};
 function callRoomSchedule() {
     if (!roomScheduleData) {
         fetch('get_all_room_schedule_data.php', {
@@ -28,14 +77,27 @@ function callRoomSchedule() {
             },
         })
         .then(response => response.json())
-        .then(rsData => {
-            roomScheduleData = (rsData); 
-            //showSchedules();
+        .then(data => {
+            roomScheduleData = (data); 
+            console.log(roomScheduleData);
+            roomSection();
         })
         .catch(error => {
             console.error('Error:', error);
         });
     } else {
-        //showSchedules();
+        roomSection();
     }
 }
+function increRoomCount() {
+    if (roomScheduleData[roomCounter+1] != undefined){
+        roomCounter += 1;
+        roomSection();
+    };
+};
+function decreRoomCount() {
+    if (roomCounter != 0) {
+        roomCounter -= 1;
+        roomSection();
+    };
+};
